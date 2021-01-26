b64b74fdab749df8a20828da15104e725304ebf5
雾架 <akinokoika@qq.com>
2021-01-26 16:05:58 +0800

    git source

--- NN_recog_num.py
+++ NN_recog_num.py
@@ -1,86 +1,28 @@
 import numpy as np
 import math
-
-def tanh(x):
-    return np.tanh(x)
-def softmax(x):
-    exp = np.exp(x-x.max())
-    return exp/exp.sum()
-
-dimensions = [28*28,10]
-activation = [tanh,softmax]
-distribution = [
-    {'b':[0,0]},
-    {'b':[0,0],
-    'w':[-math.sqrt(6/dimensions[0]+dimensions[1]),
-        math.sqrt(6/dimensions[0]+dimensions[1])]},
-]
-
-def init_parameter_b(layer):
-    dist = distribution[layer]['b']
-    return np.random.rand(dimensions[layer])*(dist[1]-dist[0])+dist[0]
-def init_parameter_w(layer):
-    dist = distribution[layer]['w']
-    return np.random.rand(dimensions[layer-1],dimensions[layer])*(dist[1]-dist[0])+dist[0]
-def init_parameter():
-    paremeter = []
-    for i in range(len(dimensions)):
-        layer_paremeter = {}
-        for j in distribution[i].keys():
-            if j == 'b':
-                layer_paremeter['b'] = init_parameter_b(i)
-                continue
-            if j == 'w':
-                layer_paremeter['w'] = init_parameter_w(i)
-                continue
-        paremeter.append(layer_paremeter)
-    return paremeter
-
-parameters = init_parameter()
-
-def predict(img,parameters):
-    l0_in = img+parameters[0]['b']
-    l0_out = activation[0](l0_in)
-    l1_in = np.dot(l0_out,parameters[1]['w'])+parameters[1]['b']
-    l1_out = activation[1](l1_in)
-    return l1_out
-    
-#print(init_parameter())
-#print(predict(np.random.rand(784),parameters))
-#print(predict(np.random.rand(784),parameters).argmax())
-
 from pathlib import Path
+import struct
+from io import BufferedReader
+from matplotlib import pyplot as plt
+import copy
 
 train_img_path = './mnist/train-images.idx3-ubyte'
 train_label_path = './mnist/train-labels.idx1-ubyte'
 test_img_path = './mnist/t10k-images.idx3-ubyte'
 test_label_path = './mnist/t10k-labels.idx1-ubyte'
 
-import struct
-import sys
-from io import BufferedReader
-
 train_num = 50000
 valid_num = 10000
 test_num = 10000
 
 def file_trans_np(file_path,sel):
-    f = open(file_path,'rb')
-    if sel == 'img':
-        struct.unpack('>4i',f.read(16))
-    if sel == 'lab':
-        struct.unpack('>2i',f.read(8))
-    if sys.platform == "linux":
-        f = BufferedReader.read(f)
+    with open(file_path,'rb') as f:
         if sel == 'img':
-            rs = np.frombuffer(f,dtype=np.uint8).reshape(-1,28*28)
+            struct.unpack('>4i',f.read(16))
+            rs = np.fromfile(f,dtype = np.uint8).reshape(-1,28*28)
         if sel == 'lab':
-            rs = np.frombuffer(f,dtype=np.uint8)
-    else:
-        if sel == 'img':
-            rs = np.fromfile(f,dtype=np.uint8).reshape(-1,28*28)
-        if sel == 'lab':
-            rs = np.fromfile(f,dtype=np.uint8)
+            struct.unpack('>2i',f.read(8))
+            rs = np.fromfile(f,dtype = np.uint8)
     return rs
 
 train_img = file_trans_np(train_img_path,'img')[:train_num]
@@ -90,10 +32,7 @@ valid_lab = file_trans_np(train_label_path,'lab')[train_num:]
 test_img = file_trans_np(test_img_path,'img')
 test_lab = file_trans_np(test_label_path,'lab')
 
-import numpy as np
-from matplotlib import pyplot as plt 
-
-def show_train(index,types):
+def show_img(index,types):
     if types == 'train':
         img = train_img[index].reshape(28,28)
         lab = train_lab[index]
@@ -106,7 +45,132 @@ def show_train(index,types):
     plt.title(f"label : {str(lab)}")
     plt.imshow(img,cmap="gray")
     plt.show()
-#show_train(0,'train')
-show_train(np.random.randint(train_num),'train')
-#show_train(np.random.randint(test_num),'test')
-#show_train(np.random.randint(valid_num),'valid')
+#show_img(np.random.randint(train_num),'train')
+#show_img(np.random.randint(test_num),'test')
+#show_img(np.random.randint(valid_num),'valid')
+
+def tanh(x):
+    return np.tanh(x)
+
+def softmax(x):
+    exp = np.exp(x - x.max())
+    return exp/exp.sum()
+
+dimensions = [28*28,10]
+activation = [tanh,softmax]
+distribution = [
+    {
+        'b':[0,0]
+    },
+    {
+        'b':[0,0],
+        'w':[-math.sqrt(6/(dimensions[0]+dimensions[1])),math.sqrt(6/(dimensions[0]+dimensions[1]))]
+    }
+]
+
+def init_parameter_b(layer):
+    dist = distribution[layer]['b']
+    return np.random.rand(dimensions[layer])*(dist[1]-dist[0])+dist[0]
+
+def init_parameter_w(layer):
+    dist = distribution[layer]['w']
+    return np.random.rand(dimensions[layer-1],dimensions[layer])*(dist[1]-dist[0])+dist[0]
+
+def init_parameters():
+    paremeter = []
+    for i in range(len(distribution)):
+        layer_paremeter = {}
+        for j in distribution[i].keys():
+            if j == 'b':
+                layer_paremeter['b'] = init_parameter_b(i)
+                continue
+            if j == 'w':
+                layer_paremeter['w'] = init_parameter_w(i)
+                continue
+        paremeter.append(layer_paremeter)
+    return paremeter
+
+#
+def predict(img,parameters):
+    l0_in = img+parameters[0]['b']
+    l0_out = activation[0](l0_in)
+    l1_in = np.dot(l0_out,parameters[1]['w'])+parameters[1]['b']
+    l1_out = activation[1](l1_in)
+    return l1_out
+
+def d_softmax(data):
+    sm = softmax(data)
+    return np.diag(sm) - np.outer(sm,sm)
+    
+def d_tanh(data):
+    return 1/(np.cosh(data))**2
+
+differential = {softmax:d_softmax,tanh:d_tanh}
+
+onehot = np.identity(dimensions[-1])
+
+def sqr_loss(img,lab,parameters):
+    y_pred = predict(img,parameters)
+    y = onehot[lab]
+    diff = y - y_pred
+    return np.dot(diff,diff)
+
+#
+def grad_parameters(img,lab,parameters):
+    l0_in = img + parameters[0]['b']
+    l0_out = activation[0](l0_in)
+    l1_in = np.dot(l0_out,parameters[1]['w'])+parameters[1]['b']
+    l1_out = activation[1](l1_in)
+
+    diff = onehot[lab] - l1_out
+    act1 = np.dot(differential[activation[1]](l1_in),diff)
+
+    grad_b1 = -2*act1
+    grad_w1 = -2*np.outer(l0_out,act1)
+    grad_b0 = -2*differential[activation[0]](l0_in)*np.dot(parameters[1]['w'],act1)
+
+    return {'b0':grad_b0,'b1':grad_b1,'w1':grad_w1}
+
+def valid_loss(parameters):
+    loss_accu = 0
+    for img_i in range(valid_num):
+        loss_accu += sqr_loss(valid_img[img_i],valid_lab[img_i],parameters)
+    return loss_accu
+    
+def valid_accuracy(parameters):
+    correct = [predict(valid_img[img_i],parameters).argmax() == valid_lab[img_i] for img_i in range(valid_num)]
+    print("valid accuracy : {}".format(correct.count(True)/len(correct)))
+
+batch_size = 100
+
+def train_batch(current_batch,parameters):
+    grad_accu = grad_parameters(train_img[current_batch*batch_size+0],train_lab[current_batch*batch_size+0],parameters)
+    for img_i in range(1,batch_size):
+        grad_tmp = grad_parameters(train_img[current_batch*batch_size+img_i],train_lab[current_batch*batch_size+img_i],parameters)
+        for key in grad_accu.keys():
+            grad_accu[key] += grad_tmp[key]
+    for key in grad_accu.keys():
+        grad_accu[key] /= batch_size
+    return grad_accu
+
+def combine_parameters(parameters,grad,learn_rate):
+    parameters_tmp = copy.deepcopy(parameters)
+    parameters_tmp[0]['b'] -= learn_rate*grad['b0']
+    parameters_tmp[1]['b'] -= learn_rate*grad['b1']
+    parameters_tmp[1]['w'] -= learn_rate*grad['w1']
+    return parameters_tmp
+
+learn_rate = 1
+parameters = init_parameters()
+valid_accuracy(parameters)
+
+for i in range(train_num//batch_size):
+    if i % 100 == 99:
+        print("batch : {}/{}".format(i+1,train_num//batch_size))
+    grad_tmp = train_batch(i,parameters)
+    parameters = combine_parameters(parameters,grad_tmp,learn_rate)
+valid_accuracy(parameters)
+
+x = np.random.randint(test_num)
+print(predict(test_img[x],parameters).argmax())
+show_img(x,'test')
